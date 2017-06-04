7591988d1c6967ff0c3be5b2f21a1e13e7637607
Juan Brito <juandebritomartins@gmail.com>
2017-06-04 19:07:18 -0300

    MazeJS
    Maze xd

---
+++ maze/mazefunctions.js
@@ -0,0 +1,140 @@
+function createCells() {
+  for (var j=0; j < rows; j++) {
+    for (var i=0; i < cols; i++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
+    }
+  }
+}
+//verifica se um elemento target tá no array array
+function inArray(target, array) {
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === target) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//retorna o index do grid (1-dimensional array)
+function index(i, j) {
+  if(i<0 || j<0 || i>cols-1 || j>rows-1) {
+    return -1;
+  }
+  return i+(j*cols);
+}
+
+function removeWalls(a, b) {
+  var x = a.i - b.i;
+  var y = a.j - b.j;
+  //tira parede esquerda de A, direita de b
+  if (x===1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  }else if(x===-1) {  //tira parede direita de a, esq. de b
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+
+  if (y===1) {  //tira parede cima de A, baixo de b
+    a.walls[0] = false;
+    b.walls[2] = false;
+  }else if(y===-1) { //tira parede baixo de A, cima de B
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
+
+function AEstrela(inicio,fim) {
+  var openSet = [inicio];
+  var closedSet = [];
+  inicio.aStarGValue = 0;
+  inicio.aStarFValue = HUEristic(inicio,fim);
+  while (openSet.length>0){
+    idxMenorOpenSetScore = menorfScore(openSet);
+    inicio = openSet[idxMenorOpenSetScore];
+    if(inicio == fim){
+      return recon_path(inicio);
+    }
+    closedSet.push(inicio);
+    openSet.splice(idxMenorOpenSetScore,1);
+    var vizinhos = inicio.retornaTodosVizinhos();
+    for (var i = 0; i<vizinhos.length; i++) {
+      if (inArray(vizinhos[i],closedSet) || !isMovePossible(inicio,vizinhos[i])){
+        continue;
+      }
+      gScore = inicio.aStarGValue +1;
+      var gScoreIsBest = false;
+      if(!inArray(vizinhos[i],openSet)){
+        gScoreIsBest = true;
+        vizinhos[i].aStarHValue = HUEristic(vizinhos[i],fim);
+        openSet.push(vizinhos[i]);
+      }else if(gScore < vizinhos[i].aStarGValue) {
+        gScoreIsBest = true;
+      }
+      if(gScoreIsBest) {
+        vizinhos[i].pai = inicio;
+        vizinhos[i].aStarGValue = gScore;
+        vizinhos[i].attFValue();
+      }
+    }
+  }
+  return [];
+}
+
+function recon_path(currentNode) {
+  var curr = currentNode;
+  var path = [];
+  while(curr.pai) {
+    path.push(curr);
+    curr = curr.pai;
+  }
+  return path.reverse();
+}
+
+function HUEristic(inicio,fim){ //euclidean distance
+  return Math.sqrt((Math.pow((fim.i - inicio.i),2) + Math.pow((fim.j-inicio.j),2)));
+}
+
+function menorfScore(set){
+  currentlow = Number.MAX_SAFE_INTEGER;
+  var idx;
+  for(var i=0;i<set.length;i++){
+    if (set[i].aStarFValue<currentlow){
+      currentlow = set.aStarFValue;
+      idx = i;
+    }
+  }
+  return idx;
+}
+
+function mostrarCaminho(caminho){
+  for(i = 0; i<caminho.length; i++){
+    caminho[i].highlightPath();
+  }
+}
+
+function isMovePossible(a, b){
+  var x = a.i - b.i;
+  var y = a.j - b.j;
+
+  if (x===1) {
+    if(a.walls[3] === false && b.walls[1] === false){
+      return true;
+    }
+  }else if(x===-1) {
+    if(a.walls[1] === false && b.walls[3] === false) {
+      return true;
+    }
+  }else if (y===1) {
+    if(a.walls[0] === false && b.walls[2] === false){
+      return true;
+    }
+  }else if(y===-1) { //tira parede baixo de A, cima de B
+    if(a.walls[2] === false && b.walls[0] === false) {
+      return true;
+    }
+  }else{
+    return false;
+  }
+}
---
+++ maze/mazesketch.js
@@ -0,0 +1,135 @@
+var cols, rows;
+var w = 20; //tamanho da cell, wid*hei deve ser divisivel por w²
+var wid = 600;
+var hei = 600;
+var totalcells = wid*hei/w/w;
+//ajusta wid e hei para caber cells de tamanho w
+while (totalcells%2){
+  wid+=1;
+  hei+=1;
+  var totalcells = wid*hei/w/w;
+}
+var grid = [];
+//muitas dessas variáveis poderiam não ser globais, porém
+//seria perdida a possibilidade de animação step-by-step
+var status = "create"; //create,solve
+var createAlg = "Prim"; //Prim,DFS
+var solveAlg = "AStar";
+var first = true;
+//para DFS
+var current;
+var stack = [];
+//para Prim
+var frontier = [];
+var frontiers;
+//para AEstrela
+var caminho;
+var fts = true;
+var openSet = [];
+var closedSet = [];
+var inicio;
+var fim;
+
+function setup() {
+  createCanvas(wid, hei);
+  cols = floor(width/w);
+  rows = floor(height/w);
+  createCells();
+  current = grid[floor(random(0, grid.length))];
+  current.visited = true;
+  frameRate(144);
+}
+
+function draw() {
+  background(51);
+  for (var i = 0; i<grid.length; i++) {
+    grid[i].show();
+  }
+  current.highlight();
+  if(status === "create") {
+    if(createAlg=="DFS") {
+      var next = current.checkNeighbors();
+      current.visited = true;
+      current.highlight();
+      if (next) {
+        stack.push(current);
+        //tira parede entre as celulas
+        removeWalls(current,next);
+        //torna o vizinho a proxima cell
+        current = next;
+      }else if(stack.length > 0){
+        current = stack.pop();
+      }else if(stack.length === 0) {
+        current = grid[0];
+        status = "solve";
+      }
+    }else if(createAlg=="Prim") {
+      current.highlight();
+
+      stack.push(current);
+      current.markNeighAsFrontier();
+      //colocar if no lugar do while se quiser animação e current e solve num else
+      if(frontier.length>0) {
+        var indiceRfc = floor(random(0,frontier.length));
+        current = frontier[indiceRfc];  //random da frontier
+        var randomIncell = current.randomInCell();
+        //stack.push(current); //comentar essa linha caso animação
+        current.visited = true;
+        current.partOffrontier = false;
+        current.markNeighAsFrontier();
+        removeWalls(current,randomIncell);
+        frontier.splice(indiceRfc,1);
+      }else{
+        current = grid[0];
+        status = "solve";
+      }
+    }
+  }else{
+    if(fts){ //first time solving
+      fim = grid[(totalcells) - 1];
+      fim.highlight();
+      inicio = current;
+      openSet = [inicio];
+      inicio.aStarGValue = 0;
+      inicio.aStarFValue = HUEristic(inicio,fim);
+      fts = false;
+    }
+    if(solveAlg=="AStar"){
+      fim.highlight();
+      if (openSet.length>0){
+        idxMenorOpenSetScore = menorfScore(openSet);
+        inicio = openSet[idxMenorOpenSetScore];
+        if(inicio == fim){
+          caminho = recon_path(inicio);
+        }
+        mostrarCaminho(recon_path(inicio));
+        closedSet.push(inicio);
+        openSet.splice(idxMenorOpenSetScore,1);
+        var vizinhos = inicio.retornaTodosVizinhos();
+        for (i = 0; i<vizinhos.length; i++) {
+          if (inArray(vizinhos[i],closedSet) || !isMovePossible(inicio,vizinhos[i])){
+            continue;
+          }
+          vizinhos[i].highlightPossible();
+          gScore = inicio.aStarGValue +1;
+          var gScoreIsBest = false;
+          if(!inArray(vizinhos[i],openSet)){
+            gScoreIsBest = true;
+            vizinhos[i].aStarHValue = HUEristic(vizinhos[i],fim);
+            openSet.push(vizinhos[i]);
+          }else if(gScore < vizinhos[i].aStarGValue) {
+            gScoreIsBest = true;
+          }
+          if(gScoreIsBest) {
+            vizinhos[i].pai = inicio;
+            vizinhos[i].aStarGValue = gScore;
+            vizinhos[i].attFValue();
+          }
+        }
+      }else{
+        mostrarCaminho(caminho);
+      }
+      //var caminho = AEstrela(current,end);
+    }
+  }
+}
